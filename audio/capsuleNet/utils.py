d8ceac131e333f1c5d35d6a4b13c0356a895c247
Duggal <duggals@amazon.com>
2018-01-13 19:22:32 +0530

    utils load-data change

--- audio/capsuleNet/utils.py
+++ audio/capsuleNet/utils.py
@@ -2,7 +2,6 @@ from PIL import Image
 #return images,labels
 
 def load_data(image_rows,image_columns,image_channels,data):
-	data = data.eval()
 	images = [Image.open(element[0]).reshape(image_rows,image_columns,image_channels) for element in data]
 	images = np.array(images)
 
